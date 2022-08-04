b9c99734dd8a2a6f9e24d009dfb5e7304789dd5f
Ely Carvalho <elycarvalho3@gmail.com>
2022-08-04 10:07:22 -0300

    Delete site-prof-Gleice2 directory

--- site-prof-Gleice2/js/script.js
+++
@@ -1,8 +0,0 @@
-function mostraHamburguer() {
-	var linksMenu = document.getElementById("conteudo-dropdown");
-	if (linksMenu.style.display === "block") {
-		linksMenu.style.display = "none";
-	} else {
-      linksMenu.style.display = "block";
-	}
-}
